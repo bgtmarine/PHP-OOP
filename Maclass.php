bcc0218f30c12e669cef3828374dd4fdba83bcb7
Marine Bigot <marinebigot.pro@gmail.com>
2023-12-12 10:58:36 +0100

    class

--- Maclass.php
+++ Maclass.php
@@ -6,6 +6,7 @@ class Maclass
    public string $attributPublic;
    public string $couleur = 'vert';
    public int $age = 54;
+   public string $nom;
    public string $prenom;
    //en PHP les constante sont toujour en majuscule
    public const PI=3.14;
@@ -16,47 +17,60 @@ class Maclass
    private string $_nom = 'Michel PLIK';
 
 
-   //Méthode
-   //Méthode Public
-   //Methode 1
-   public function displayMethode($value)
+   public function __construct($nom,$prenom)
    {
-    //deux possibilité echo ou return
-// echo 'Jesuis une Methode qui affiche ' . $value;
-return $this->prenom . 'aime la couleur ' . $this->couleur .' '. $value;
+       $this->nom = $nom;
+       $this->prenom =$prenom;
    }
 
-   //Methode 2
-public function calculCercleAire(): float|int
-{
-    //self permet (mot cles) de representer la class dans laquelle tu te trouve et va chercher les infos dans cette classe
-    //echo self::PI;
-    return self::PI * $this->rayon * $this->rayon;
+   public function affichePerson()
+   {
+    return $this->nom . ' ' . $this->prenom;
+   }
 }
+//    //Méthode
+//    //Méthode Public
+//    //Methode 1
+//    public function displayMethode($value)
+//    {
+//     //deux possibilité echo ou return
+// // echo 'Jesuis une Methode qui affiche ' . $value;
+// return $this->prenom . 'aime la couleur ' . $this->couleur .' '. $value;
+//    }
+
+//    //Methode 2
+// public function calculCercleAire(): float|int
+// {
+//     //self permet (mot cles) de representer la class dans laquelle tu te trouve et va chercher les infos dans cette classe
+//     //echo self::PI;
+//     return self::PI * $this->rayon * $this->rayon;
+// }
    
-public function calculCercleAire2(float|int $rayonParam) : float|int
-{
-    return self::PI * $rayonParam * $rayonParam;
-}
+// public function calculCercleAire2(float|int $rayonParam) : float|int
+// {
+//     return self::PI * $rayonParam * $rayonParam;
+// }
    
-}
-//création d'objet
-$obj = new Maclass();
-//quand on fait appel à un attribut on n'utilise pas le $
-// echo 'Couleur: ' . $obj->couleur . '<br>';
-// echo 'Age: '. $obj-> age . '<br>';
-//cela ne fonctionne pas car on ne peiut accéder à un attribut privée
-// echo 'Nom: '. $obj-> _nom . '<br>';
-$obj->prenom = 'Bill ';
+// }
+// //création d'objet
+// $obj = new Maclass();
+// //quand on fait appel à un attribut on n'utilise pas le $
+// // echo 'Couleur: ' . $obj->couleur . '<br>';
+// // echo 'Age: '. $obj-> age . '<br>';
+// //cela ne fonctionne pas car on ne peiut accéder à un attribut privée
+// // echo 'Nom: '. $obj-> _nom . '<br>';
+// $obj->prenom = 'Bill ';
 
-//Methode 1
-//si echo de pas mettre echo devant
-//si return mettre un echo devant
-echo $obj->displayMethode('');
+// //Methode 1
+// //si echo de pas mettre echo devant
+// //si return mettre un echo devant
+// echo $obj->displayMethode('');
 
-//Methode 2
-$obj->rayon = 5;
-echo $obj->calculCercleAire() . ' cm2 ';
+// //Methode 2
+// $obj->rayon = 5;
+// echo $obj->calculCercleAire() . ' cm2 ';
 
-echo $obj->calculCercleAire2(5) . ' cm2';
+// echo $obj->calculCercleAire2(5) . ' cm2';
 
+$person = new MaClass('jon','doe');
+echo $person->affichePerson();
